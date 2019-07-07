eed75b9048b053a45353c2de7d8a3f74793b396b
jvgiv <jvgiv12@gmail.com>
2019-07-07 14:03:26 -0400

    Basic Layout setup complete

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, NavLink, withRouter } from 'react-router-dom'
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello Faggot</h1>
+    </div>
+  );
+}
+
+
+export default withRouter(App);
